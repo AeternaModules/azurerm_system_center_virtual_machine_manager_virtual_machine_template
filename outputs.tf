7742e7c5ef485e5bdeedc9980c6ff0d1644ade42
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:21:14 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,25 @@
-output "system_center_virtual_machine_manager_virtual_machine_templates" {
-  description = "All system_center_virtual_machine_manager_virtual_machine_template resources"
-  value       = azurerm_system_center_virtual_machine_manager_virtual_machine_template.system_center_virtual_machine_manager_virtual_machine_templates
-}
 output "system_center_virtual_machine_manager_virtual_machine_templates_custom_location_id" {
-  description = "List of custom_location_id values across all system_center_virtual_machine_manager_virtual_machine_templates"
-  value       = [for k, v in azurerm_system_center_virtual_machine_manager_virtual_machine_template.system_center_virtual_machine_manager_virtual_machine_templates : v.custom_location_id]
+  description = "Map of custom_location_id values across all system_center_virtual_machine_manager_virtual_machine_templates, keyed the same as var.system_center_virtual_machine_manager_virtual_machine_templates"
+  value       = { for k, v in azurerm_system_center_virtual_machine_manager_virtual_machine_template.system_center_virtual_machine_manager_virtual_machine_templates : k => v.custom_location_id }
 }
 output "system_center_virtual_machine_manager_virtual_machine_templates_location" {
-  description = "List of location values across all system_center_virtual_machine_manager_virtual_machine_templates"
-  value       = [for k, v in azurerm_system_center_virtual_machine_manager_virtual_machine_template.system_center_virtual_machine_manager_virtual_machine_templates : v.location]
+  description = "Map of location values across all system_center_virtual_machine_manager_virtual_machine_templates, keyed the same as var.system_center_virtual_machine_manager_virtual_machine_templates"
+  value       = { for k, v in azurerm_system_center_virtual_machine_manager_virtual_machine_template.system_center_virtual_machine_manager_virtual_machine_templates : k => v.location }
 }
 output "system_center_virtual_machine_manager_virtual_machine_templates_name" {
-  description = "List of name values across all system_center_virtual_machine_manager_virtual_machine_templates"
-  value       = [for k, v in azurerm_system_center_virtual_machine_manager_virtual_machine_template.system_center_virtual_machine_manager_virtual_machine_templates : v.name]
+  description = "Map of name values across all system_center_virtual_machine_manager_virtual_machine_templates, keyed the same as var.system_center_virtual_machine_manager_virtual_machine_templates"
+  value       = { for k, v in azurerm_system_center_virtual_machine_manager_virtual_machine_template.system_center_virtual_machine_manager_virtual_machine_templates : k => v.name }
 }
 output "system_center_virtual_machine_manager_virtual_machine_templates_resource_group_name" {
-  description = "List of resource_group_name values across all system_center_virtual_machine_manager_virtual_machine_templates"
-  value       = [for k, v in azurerm_system_center_virtual_machine_manager_virtual_machine_template.system_center_virtual_machine_manager_virtual_machine_templates : v.resource_group_name]
+  description = "Map of resource_group_name values across all system_center_virtual_machine_manager_virtual_machine_templates, keyed the same as var.system_center_virtual_machine_manager_virtual_machine_templates"
+  value       = { for k, v in azurerm_system_center_virtual_machine_manager_virtual_machine_template.system_center_virtual_machine_manager_virtual_machine_templates : k => v.resource_group_name }
 }
 output "system_center_virtual_machine_manager_virtual_machine_templates_system_center_virtual_machine_manager_server_inventory_item_id" {
-  description = "List of system_center_virtual_machine_manager_server_inventory_item_id values across all system_center_virtual_machine_manager_virtual_machine_templates"
-  value       = [for k, v in azurerm_system_center_virtual_machine_manager_virtual_machine_template.system_center_virtual_machine_manager_virtual_machine_templates : v.system_center_virtual_machine_manager_server_inventory_item_id]
+  description = "Map of system_center_virtual_machine_manager_server_inventory_item_id values across all system_center_virtual_machine_manager_virtual_machine_templates, keyed the same as var.system_center_virtual_machine_manager_virtual_machine_templates"
+  value       = { for k, v in azurerm_system_center_virtual_machine_manager_virtual_machine_template.system_center_virtual_machine_manager_virtual_machine_templates : k => v.system_center_virtual_machine_manager_server_inventory_item_id }
 }
 output "system_center_virtual_machine_manager_virtual_machine_templates_tags" {
-  description = "List of tags values across all system_center_virtual_machine_manager_virtual_machine_templates"
-  value       = [for k, v in azurerm_system_center_virtual_machine_manager_virtual_machine_template.system_center_virtual_machine_manager_virtual_machine_templates : v.tags]
+  description = "Map of tags values across all system_center_virtual_machine_manager_virtual_machine_templates, keyed the same as var.system_center_virtual_machine_manager_virtual_machine_templates"
+  value       = { for k, v in azurerm_system_center_virtual_machine_manager_virtual_machine_template.system_center_virtual_machine_manager_virtual_machine_templates : k => v.tags }
 }
 
